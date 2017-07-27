fcafbf73ed493b977af4c24f1f0a9925b7a5e1b0
nooras <ansarinoorasfatima@gmail.com>
2017-07-28 00:15:25 +0530

    evaluation of postfix

---
+++ postfixexp.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<ctype.h>
+typedef struct conversion
+{
+	int a[30];
+	int  top;
+}stack;
+void push(stack *s,int);
+int pop(stack *s);
+int operation(int num1,int num2,char opr);
+int evaluate(char pos[]);
+int main()
+{
+	char postfix[30];
+	printf("\nENTER POSTFIX EXPRESSION");
+	scanf("%s",postfix);
+	printf("\nEVALUATED POSTFIX EXPRESSION=%d",evaluate(postfix));
+	return 0;
+}
+
+void push(stack *s,int opr)
+{
+		if(s->top<29)
+		{	s->top++;
+			s->a[s->top]=opr;
+		}
+}
+int pop(stack *s)
+{
+	if (s->top==-1)
+	{
+		printf("stack is empty\n");
+		return 0;
+    }
+    else
+    {
+		int data=s->a[s->top--];
+		return data;
+    }
+}
+int operation(int num1,int num2,char opr)
+{
+	switch(opr)
+	{
+		case'+':
+		return num1+num2;
+		break;
+		case'-':
+		return num1-num2;
+		break;
+		case'*':
+		return num1*num2;
+		break;
+		case'/':
+		return num1/num2; 
+	}
+}
+	
+int evaluate(char pos[30])
+{
+	int i,n1,n2,result,opr;
+	stack s;
+	for(i=0;pos[i]!='\0';i++)
+	{
+		opr=pos[i];
+		if(isdigit(opr))
+		push(&s,opr-'0');
+		else
+		{
+			n1=pop(&s);
+			n2=pop(&s);
+			result=operation(n1,n2,opr);
+			push(&s,result);
+		}
+	}
+	return pop(&s);
+}
+
