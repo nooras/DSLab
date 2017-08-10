02078dab39ac4e4aaa4f2dde53956063dc412b3b
nooras <ansarinoorasfatima@gmail.com>
2017-08-11 00:33:14 +0530

    Circular queue

---
+++ CircularQueue.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#define MAXSIZE 10
+int Queue[MAXSIZE],front=-1,rear=-1;
+int isFull()
+{
+	//return 1 if  is full else return 0
+	if(((abs(front-rear) % (MAXSIZE - 1)==1) || (abs(front-rear) % (MAXSIZE-1)==0)) && (front!=-1 && rear!=-1))
+		return 1;
+	else
+		return 0;
+}
+int isEmpty(){
+	//return 1 if Queue is empty else return -0
+	if(front == -1 && rear == -1)
+		return 1;
+	else 
+		return 0;
+}
+int peek()
+{
+	//return element at the front of Queue 
+	return Queue[front];
+}	
+void insertQueue(int d)
+{
+	if(!isFull())
+	{
+		if(isEmpty())
+		{
+			front++;
+		}
+			rear=(rear++) % MAXSIZE;
+		printf("ELEMENT INSERT SUCCESSFULLY %d\n",d);
+	}
+	else
+		printf("QUEUE IS FULL ");
+}
+void deleteQueue()
+{
+	if(!isEmpty())
+	{
+		int d=Queue[front];
+		if(front==rear)
+			front=rear=-1;
+		else 
+			front=(front++) % MAXSIZE;
+		printf("THE ELEMENT DELETED IS %d",d);
+	}	
+	else
+		printf("QUEUE IS EMPty");
+}
+int main()
+{
+	int choice,e;
+	while(1)
+	{
+	    printf("1.Display 2.INSERT 3.DELETE 4.EXIT\n");
+		printf("ENTER CHOICE\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				if(isEmpty())
+					printf("QUEUE IS EMPTY");
+				else
+					printf("ELEMENT IS FRONT OF QUEUE IS %d",peek());
+				break;
+			case 2:
+				printf("ENTER ELEMENT TO INSERT");
+				scanf("%d",&e);
+				insertQueue(e);
+				break;
+			case 3:
+				deleteQueue();
+				break;
+			case 4:
+				exit(0);
+				break;
+			default:
+				printf("ENTER INVALID CHOICE");
+		}
+	}
+}
